a4980a3cc24d0a36b09da0cce34f04f501235180
oholiba <gallerandreas@protonmail.com>
2021-05-11 18:06:01 +0200

    adapt to recent changes in litex files

--- arch/risc-v/src/ibex/ibex_irq.c
+++ arch/risc-v/src/ibex/ibex_irq.c
@@ -58,10 +58,9 @@ void up_irqinitialize(void)
 
   /* Colorize the interrupt stack for debug purposes */
 
-#if defined(CONFIG_STACK_COLORATION) && CONFIG_ARCH_INTERRUPTSTACK > 3
-  size_t intstack_size = (CONFIG_ARCH_INTERRUPTSTACK & ~3);
-  riscv_stack_color((FAR void *)((uintptr_t)&g_intstackbase - intstack_size),
-                 intstack_size);
+#if defined(CONFIG_STACK_COLORATION) && CONFIG_ARCH_INTERRUPTSTACK > 15
+  size_t intstack_size = (CONFIG_ARCH_INTERRUPTSTACK & ~15);
+  riscv_stack_color((FAR void *)&g_intstackalloc, intstack_size);
 #endif
 
 
